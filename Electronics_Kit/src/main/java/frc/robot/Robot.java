f437b307d7e196c144d233380871c4f0084cbd20
Mboesn <moshechelsky@gmail.com>
2020-01-12 19:09:58 +0200

    reverted master

--- Electronics_Kit/src/main/java/frc/robot/Robot.java
+++ Electronics_Kit/src/main/java/frc/robot/Robot.java
@@ -1,33 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.MoveMotors;
-import frc.robot.subsystems.Motors;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  public static OI oi;
-  public static Motors motorOne;
-  public static Motors motorTwo;
-  public static Motors motorThree;
-  public static Motors motorOneAndThree;
 
+  private RobotContainer m_robotContainer;
 
-
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-    Robot.motorOne = new Motors(RobotMap.VICTOR_SP_1);
-    Robot.motorTwo = new Motors(RobotMap.VICTOR_SP_2);
-    Robot.motorThree = new Motors(RobotMap.VICTOR_SP_3);
-    Robot.motorOneAndThree = new Motors(RobotMap.VICTOR_SP_1, RobotMap.VICTOR_SP_3);
-    Robot.oi = new OI();
-    
-    SmartDashboard.putData("move motors", new MoveMotors(motorOne, oi.getXboxController()::getY, () -> false));
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
   @Override
   public void robotPeriodic() {
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
@@ -37,6 +49,9 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().run();
   }
 
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
   @Override
   public void disabledInit() {
   }
@@ -45,8 +60,12 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {
   }
 
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -54,6 +73,9 @@ public class Robot extends TimedRobot {
     }
   }
 
+  /**
+   * This function is called periodically during autonomous.
+   */
   @Override
   public void autonomousPeriodic() {
   }
@@ -69,6 +91,9 @@ public class Robot extends TimedRobot {
     }
   }
 
+  /**
+   * This function is called periodically during operator control.
+   */
   @Override
   public void teleopPeriodic() {
   }
@@ -79,6 +104,9 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().cancelAll();
   }
 
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
   public void testPeriodic() {
   }
