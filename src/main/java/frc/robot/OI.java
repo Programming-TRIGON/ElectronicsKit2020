c6b27586359b6201002815fb1b4a2ac36fd093ea
Mboesn <moshechelsky@gmail.com>
2020-01-13 19:31:11 +0200

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,18 +1,22 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.MoveMotors;
 
 /**
- * This class is the place where we do the joystick stuff 
+ * This class is the place where we do the joystick stuff
  */
 public class OI {
     private XboxController driver;
 
-    public OI(){
+    public OI() {
         this.driver = new XboxController(0);
+
+        driver.getAButtonPressed(new MoveMotors(Robot.motorOneAndThree, () -> 0.6, driver::getAButtonReleased));
     }
 
-    public XboxController getXboxController(){
+    public XboxController getXboxController() {
         return this.driver;
     }
+
 }
