beaba3ab4ac6e58bfa9631a6ff323c5e4e2308ea
Mboesn <moshechelsky@gmail.com>
2020-01-18 20:37:48 +0200

    removed pid talon

--- src/main/java/frc/robot/subsystems/Motors.java
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,21 +10,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * Creates a new Motor, and hold all the functions of said motor.
  */
 public class Motors extends SubsystemBase {
-  private SpeedController motors[];
+  private WPI_TalonSRX motors[];
 
-  public Motors(SpeedController... motors) {
+  public Motors(WPI_TalonSRX... motors) {
     this.motors = motors;
   }
 
-  public Motors(CommandBase command, SpeedController... motors) {
+  public Motors(CommandBase command, WPI_TalonSRX... motors) {
     this(motors);
     command.addRequirements(this);
     setDefaultCommand(command);
+    
   }
 
   public void moveMotors(double power) {
     for (SpeedController speedController : this.motors) {
       speedController.set(power);
     }
+
   }
 }
